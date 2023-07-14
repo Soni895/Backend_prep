0f4c7a16f1f20b173306d139c75fbdeae60c6263
soni895 <darshansoni895@gmail.com>
2023-07-14 23:35:20 +0530

    post call initiated

--- Server.js
+++ Server.js
@@ -1,10 +1,28 @@
 const express =require("express");
-const app= express();  
+const app= express(); 
+
+//ue to parse req.body in express use in --> put or post
+const bodyparser=require("body-parser") ;
+
+//specificall parse json data and add it to req.body object
+app.use(bodyparser.json());
 
 app.listen(3000,()=>
-(console.log("server started at 3000 port"))
+(console.log("server started at 300 port"))
 );
 // Define a route
 app.get('/', (req, res) => {
-    res.send('hi');
+    res.send('hi inside the get call');
   });
+
+  //post request
+
+  app.post("/home",(req,res)=>
+  {
+    const {name,rollno}=req.body;
+    console.log(name,rollno);
+
+   res.send('hi inside the post call');
+
+  }
+  );
