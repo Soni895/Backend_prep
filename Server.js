20b11514e024f03052ecd49e1257ad52eee624c3
soni895 <darshansoni895@gmail.com>
2023-07-14 18:57:01 +0530

    server live

--- Server.js
+++ Server.js
@@ -2,5 +2,9 @@ const express =require("express");
 const app= express();  
 
 app.listen(3000,()=>
-(console.log("server started at 3001 port"))
+(console.log("server started at 3000 port"))
 );
+// Define a route
+app.get('/', (req, res) => {
+    res.send('hi');
+  });
