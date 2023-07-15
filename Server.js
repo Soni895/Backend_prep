2ed0e494af54f4358ccc3ed908a09eee0d0dc8e9
soni895 <darshansoni895@gmail.com>
2023-07-15 23:31:44 +0530

    revesion

--- Server.js
+++ Server.js
@@ -1,28 +1,56 @@
-const express =require("express");
-const app= express(); 
+// const express =require("express");
+// const app= express(); 
 
-//ue to parse req.body in express use in --> put or post
+// //ue to parse req.body in express use in --> put or post
+// const bodyparser=require("body-parser") ;
+
+// //specificall parse json data and add it to req.body object
+// app.use(bodyparser.json());
+
+// app.listen(3000,()=>
+// (console.log("server started at 300 port"))
+// );
+// // Define a route
+// app.get('/', (req, res) => {
+//     res.send('hi inside the get call');
+//   });
+
+//   //post request
+
+//   app.post("/home",(req,res)=>
+//   {
+//     const {name,rollno}=req.body;
+//     console.log(name,rollno);
+
+//    res.send('hi inside the post call');
+
+//   }
+//   );
+
+
+const express=require("express");
+const app=express();
+
 const bodyparser=require("body-parser") ;
-
-//specificall parse json data and add it to req.body object
 app.use(bodyparser.json());
 
 app.listen(3000,()=>
-(console.log("server started at 300 port"))
+
+
+{
+  console.log("hi");
+}
+
 );
-// Define a route
-app.get('/', (req, res) => {
-    res.send('hi inside the get call');
-  });
+app.get('/home',(req,res)=>
+{
+  res.send("hi inside the home");
+})
 
-  //post request
-
-  app.post("/home",(req,res)=>
-  {
-    const {name,rollno}=req.body;
-    console.log(name,rollno);
-
-   res.send('hi inside the post call');
-
-  }
-  );
+app.post("/dashboard",(req,res)=>
+{
+  const {name,rollno}=req.body;
+  console.log(name,rollno);
+  res.send("successfull print data");
+}
+)
