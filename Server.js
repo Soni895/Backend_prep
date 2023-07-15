9e4b6eb0336cfd30e07f9708eb1d5e27473e4a6a
soni895 <darshansoni895@gmail.com>
2023-07-16 00:52:09 +0530

    connection established

--- Server.js
+++ Server.js
@@ -54,3 +54,16 @@ app.post("/dashboard",(req,res)=>
   res.send("successfull print data");
 }
 )
+
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/info",
+{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+}).then(()=>
+{
+  console.log("connection succes ful")
+},(error)=>
+{
+  console.log("connection failed");
+})
