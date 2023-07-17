a77a3e0992cb4b24f6a49faf8537c3055ab50e1c
soni895 <darshansoni895@gmail.com>
2023-07-17 20:45:00 +0530

    code updated

--- Server.js
+++ Server.js
@@ -1,66 +1,68 @@
-// // const express =require("express");
-// // const app= express(); 
+// // // const express =require("express");
+// // // const app= express(); 
 
-// // //ue to parse req.body in express use in --> put or post
-// // const bodyparser=require("body-parser") ;
+// // // //ue to parse req.body in express use in --> put or post
+// // // const bodyparser=require("body-parser") ;
 
-// // //specificall parse json data and add it to req.body object
-// // app.use(bodyparser.json());
+// // // //specificall parse json data and add it to req.body object
+// // // app.use(bodyparser.json());
 
-// // app.listen(3000,()=>
-// // (console.log("server started at 300 port"))
-// // );
-// // // Define a route
-// // app.get('/', (req, res) => {
-// //     res.send('hi inside the get call');
-// //   });
+// // // app.listen(3000,()=>
+// // // (console.log("server started at 300 port"))
+// // // );
+// // // // Define a route
+// // // app.get('/', (req, res) => {
+// // //     res.send('hi inside the get call');
+// // //   });
 
-// //   //post request
+// // //   //post request
 
-// //   app.post("/home",(req,res)=>
-// //   {
-// //     const {name,rollno}=req.body;
-// //     console.log(name,rollno);
+// // //   app.post("/home",(req,res)=>
+// // //   {
+// // //     const {name,rollno}=req.body;
+// // //     console.log(name,rollno);
 
-// //    res.send('hi inside the post call');
+// // //    res.send('hi inside the post call');
 
-// //   }
-// //   );
-
-
-const express=require("express");
-const app=express();
-
-const bodyparser=require("body-parser") ;
-app.use(bodyparser.json());
-
-app.listen(3001,()=>
+// // //   }
+// // //   );
 
 
-{
-  console.log("hi");
-}
+// const express=require("express");
+// const app=express();
 
-);
-app.get('/home',(req,res)=>
-{
-  res.send("hi inside the home");
-})
+// const bodyparser=require("body-parser") ;
+// app.use(bodyparser.json());
 
-app.post("/dashboard",(req,res)=>
-{
-  const {name,rollno}=req.body;
-  console.log(name,rollno);
-  res.send("successfull print data");
-}
-)
+// app.listen(3000,()=>
 
-const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/mydataabse",
-{
+
+// {
+//   console.log("hi");
+// }
+
+// );
+// app.get('/home',(req,res)=>
+// {
+//   res.send("hi inside the home");
+// })
+
+// app.post("/dashboard",(req,res)=>
+// {
+//   const {name,rollno}=req.body;
+//   console.log(name,rollno);
+//   res.send("successfull print data");
+// }
+// )
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mydataabse'
+
+,{
   useNewUrlParser:true,
   useUnifiedTopology:true
-})
+}
+)
 .then(()=>
 {
   console.log("connection successful")
@@ -72,5 +74,4 @@ mongoose.connect("mongodb://localhost:27017/mydataabse",
     console.log(error);
   } 
 )
-
 
